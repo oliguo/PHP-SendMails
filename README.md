29d9263b43a17025810ba10ac2bd1906aff3519d
oliverguo <oliverguo@outlook.com>
2016-02-16 17:02:18 +0800

    add outlook to send mails
    add outlook to send mails

--- README.md
+++ README.md
@@ -7,6 +7,36 @@ There are some issue to fix problems if you send mails by google.
 Google Detail:
 <br/>
 <a href="http://stackoverflow.com/questions/21937586/phpmailer-smtp-error-password-command-failed-when-send-mail-from-my-server">link</a>
+<br/>
+
+outlook:
+<br/>
+I figure out some issue by:
+
+tutorial:<a href="http://subinsb.com/send-mails-via-smtp-server-gmail-outlook-php">link</a>
+
+config ref:<a href="http://windows.microsoft.com/en-us/windows/outlook/send-receive-from-app">link</a>
+
+If get '535 5.0.0 Authentication Failed' ,please go to
+approve the activity by <a href="https://account.live.com/activity">link</a>
+<br/>
+ref:<a href="http://answers.microsoft.com/en-us/outlook_com/forum/osecurity-osafe/windows-live-mail-not-working-properly-535-521/42c342ec-08e5-4899-8bb4-a4b5344055ce">link</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<br/>
+Pls notice: send mails only work on server not localhost
 
 
 
--- sendMail_outlook.php
+++ sendMail_outlook.php
@@ -1,5 +1,4 @@
-<pre>
-<?php
+<pre><?php
 
 //include phpmailer
 //require_once 'PHPMailer/PHPMailerAutoload.php';  
@@ -8,17 +7,17 @@ require ("PHPMailer/class.smtp.php");
 $email = "who@who.com";
 //define the receiver of the email
 $mail = new PHPMailer();                          //establish a new PHPmailer  
-$mail->SMTPDebug = 2;                          //sendmail error log
+$mail->SMTPDebug = 3;                          //sendmail error log
 $mail->IsSMTP();                                 //設定使用SMTP方式寄信  
 $mail->SMTPAuth = true;                          //設定SMTP需要驗證  
-$mail->SMTPSecure = "ssl";                       // SMTP主機需要使用SSL連線  
-$mail->Host = "smtp.gmail.com";                  //SMTP主機  
-$mail->Port = 465;                               //SMTP主機的埠號(Gmail為465)。  
+$mail->SMTPSecure = "tls";                       // SMTP主機需要使用SSL連線  
+$mail->Host = "smtp-mail.outlook.com";                  //SMTP主機  
+$mail->Port = 587;                               //SMTP主機的埠號(Gmail為465)。  
 $mail->CharSet = "utf-8";                        //郵件編碼  
-$mail->Username = "youraccount@gmail.com";      //帳號  
+$mail->Username = "youraccount@outlook.com";      //帳號  
 $mail->Password = "yourpassword";                   //密碼  
-$mail->From = "from@gmail.com";           //寄件者信箱  
-$mail->FromName = "fromName";                    //寄件者姓名  
+$mail->From = "youraccount@outlook.com";           //寄件者信箱  
+$mail->FromName = "testing";                    //寄件者姓名  
 $mail->Subject = "Email testing 中文是否亂碼";             //郵件標題  
 $mail->Body = "<p>testing by <b>VTL</b><small>中文是否亂碼</small></p>";  //mail content  
 $mail->IsHTML(true);                             //郵件內容為html  
@@ -30,5 +29,4 @@ if (!$mail->Send()) {
 }  
 ?>
 </pre>
-     
    
